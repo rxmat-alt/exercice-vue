4f055203d04b92b3886d4ccf8c57caa4adf3f102
Surya Ambrose <surya.ambrose@gmail.com>
2021-05-22 22:35:32 +0200

    Fix task store

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -4,7 +4,7 @@ const tasksModule = {
       {
         id: 1,
         image: "https://i.imgur.com/0umadnY.jpg",
-        nom: "Burger",
+        name: "Burger",
         description:
           "Un hamburger est un sandwich composé d'une ou plusieurs tranches de viande hachée, généralement du bœuf, placées dans un petit pain ou une brioche.",
         note: 4
@@ -12,7 +12,7 @@ const tasksModule = {
       {
         id: 2,
         image: "https://i.imgur.com/b9zDbyb.jpg",
-        nom: "Pizza",
+        name: "Pizza",
         description:
           "La pizza est un plat savoureux d'origine italienne, consistant en une base généralement ronde et aplatie de pâte levée à base de blé.",
         note: 5
@@ -20,7 +20,7 @@ const tasksModule = {
       {
         id: 3,
         image: "https://i.imgur.com/RbKjUjB.jpg",
-        nom: "Petits choux",
+        name: "Petits choux",
         description:
           "Le chou de Bruxelles est une variété de chou, plante herbacée de la famille des Brassicaceae. C’est vraiement pas bon...",
         note: 1
@@ -28,7 +28,7 @@ const tasksModule = {
       {
         id: 4,
         image: "https://i.imgur.com/xAuhNVg.jpg",
-        nom: "BBQ Ribs",
+        name: "BBQ Ribs",
         description:
           "Les BBQ ribs ou barbecue ribs sont des grands classiques très appréciés partout dans le monde.",
         note: 5
